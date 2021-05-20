2c9b6a5d33957493507697be386acd254c454f52
AlessioDP <alessiodptv@gmail.com>
2021-05-20 15:54:55 +0200

    Added PartiesAPI check

--- src/main/java/com/alessiodp/partiesexample/bungeecord/PartiesExample.java
+++ src/main/java/com/alessiodp/partiesexample/bungeecord/PartiesExample.java
@@ -1,5 +1,7 @@
 package com.alessiodp.partiesexample.bungeecord;
 
+import com.alessiodp.parties.api.Parties;
+import com.alessiodp.parties.api.interfaces.PartiesAPI;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class PartiesExample extends Plugin {
@@ -8,6 +10,9 @@ public class PartiesExample extends Plugin {
 		if (getProxy().getPluginManager().getPlugin("Parties") != null) {
 			getProxy().getPluginManager().registerListener(this, new Listeners());
 			System.out.println("Hooked into Parties");
+			
+			PartiesAPI partiesApi = Parties.getApi();
+			System.out.println("PartiesAPI instance: " + partiesApi);
 		} else {
 			System.out.println("Cannot hook into Parties");
 		}
